c65483aedbab583d7b3817ead1966d4047ac47ce
Milad Xandi <miladxandi@outlook.com>
2025-04-21 05:29:05 +0330

    OK

---
+++ CMakeFiles/Ciple-Framework.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ciple-Framework.dir/App/Http/Controller/HomeController.cpp.o"
+  "CMakeFiles/Ciple-Framework.dir/App/Http/Controller/HomeController.cpp.o.d"
+  "CMakeFiles/Ciple-Framework.dir/main.cpp.o"
+  "CMakeFiles/Ciple-Framework.dir/main.cpp.o.d"
+  "Ciple-Framework"
+  "Ciple-Framework.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ciple-Framework.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/Ciple-Framework.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Ciple-Framework.
+# This may be replaced when dependencies are built.
