3e5fc8c5484a3b2dc82239296d38edacdb2a718a
Milad Xandi <miladxandi@outlook.com>
2025-04-20 22:39:07 -0700

    ok

--- main.cpp
+++ main.cpp
@@ -5,36 +5,42 @@
 #include "Routes/Api.h"
 #include "Helpers/Fs/Dotenv.h"
 #include <sentry.h>
-int main() {
 
+int main() {
     sentry_options_t *options = sentry_options_new();
     sentry_options_set_dsn(options, "https://63e9996dd42969649cf294a4847b1934@o4508958548557824.ingest.de.sentry.io/4508958678843472");
+    // This is also the default-path. For further information and recommendations:
+    // https://docs.sentry.io/platforms/native/configuration/options/#database-path
     sentry_options_set_database_path(options, ".sentry-native");
-    sentry_options_set_release(options, "server@2.3.12");
+    sentry_backend_t * snt = nullptr;
+    sentry_options_set_backend(options, snt); // یا SENTRY_BACKEND_BREAKPAD
+    sentry_options_set_release(options, "my-project-name@2.3.12");
     sentry_options_set_debug(options, 1);
+    sentry_options_set_auto_session_tracking(options, 1);
 
     sentry_init(options);
 
     sentry_capture_event(sentry_value_new_message_event(
-  /*   level */ SENTRY_LEVEL_INFO,
-  /*  logger */ "custom",
-  /* message */ "It works!"
+            /*   level */ SENTRY_LEVEL_INFO,
+            /*  logger */ "custom",
+            /* message */ "It works!"
     ));
 
+
+    sentry_start_session();
     // make sure everything flushes
-    sentry_close();
 
-    dotenv::init("../../.env");
-    password = dotenv::getenv("PASSWORD");
-    public_key = loadKey(dotenv::getenv("PUBLIC_KEY"));
-    private_key = loadKey(dotenv::getenv("PRIVATE_KEY"));
+        dotenv::init("../../.env");
+        password = dotenv::getenv("PASSWORD");
+        public_key = loadKey(dotenv::getenv("PUBLIC_KEY"));
+        private_key = loadKey(dotenv::getenv("PRIVATE_KEY"));
 
-    SimpleApp app;
-    set_global_base(dotenv::getenv("TEMPLATES"));
+        SimpleApp app;
+        set_global_base(dotenv::getenv("TEMPLATES"));
 
-    addWebRoute(app);
-    addApiRoute(app);
+        addWebRoute(app);
+        addApiRoute(app);
 
-    app.port(18080).run();
-
+        app.port(18080).run();
+    sentry_end_session();
 }
