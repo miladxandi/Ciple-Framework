337b3f88462622fb37c537410e11da6a95cb4f5a
Milad Xandi <miladxandi@outlook.com>
2025-04-21 07:06:38 +0330

    OK

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main() {
     // https://docs.sentry.io/platforms/native/configuration/options/#database-path
     sentry_options_set_database_path(options, ".sentry-native");
     sentry_options_set_release(options, "my-project-name@2.3.12");
-    sentry_options_set_handler_path(options, "bin/crashpad_handler");
+    sentry_options_set_backend(options, "inproc");
     sentry_options_set_debug(options, 1);
     sentry_init(options);
 
