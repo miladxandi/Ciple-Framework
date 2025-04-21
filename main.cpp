e6810f8db7f8db57c71daacf434dc2ceb57ca074
Milad Xandi <miladxandi@outlook.com>
2025-04-21 06:40:44 +0330

    OK

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@
 int main() {
     // Initialize Sentry
     sentry_options_t *options = sentry_options_new();
-    sentry_options_set_dsn(options, "YOUR_DSN_HERE");
+    sentry_options_set_dsn(options, "https://63e9996dd42969649cf294a4847b1934@o4508958548557824.ingest.de.sentry.io/4508958678843472");
+    // This is also the default-path. For further information and recommendations:
+    // https://docs.sentry.io/platforms/native/configuration/options/#database-path
     sentry_options_set_database_path(options, ".sentry-native");
     sentry_options_set_release(options, "my-project-name@2.3.12");
     sentry_options_set_handler_path(options, "bin/crashpad_handler");
